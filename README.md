ce318c5232b3d94888d61ed6bac38cbd8643284d
gitlab.com/pepa65 <pepa65@passchier.net>
2026-02-16 00:32:10 +0700

    First tagged release: v0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# mvimap
+[![Go Report Card](https://goreportcard.com/badge/github.com/pepa65/mvimap)](https://goreportcard.com/report/github.com/pepa65/mvimap)
+[![GoDoc](https://godoc.org/github.com/pepa65/mvimap?status.svg)](https://godoc.org/github.com/pepa65/mvimap)
+[![GitHub](https://img.shields.io/github/license/pepa65/mvimap.svg)](LICENSE)
+# mvimap v0.1.0
 **Move (or copy) messages from IMAP to IMAP**
 
 * Leveraging [go-imap](https://github.com/emersion/go-imap)
@@ -8,14 +11,54 @@
 
 ## Usage
 ### Keep moving IMAP Inbox to IMAP Inbox
-`mvimap -from example.com:993 -fromuser foo@example.com -frompw himom -to other.example.com:993 -touser bar@other.example.com -topw hidad`
+`mvimap example.com:993 foo@example.com himom other.example.com:993 bar@other.example.com hidad`
 
 ### Move IMAP Inbox to IMAP Inbox Once
-`mvimap -once -from example.com:993 -fromuser foo@example.com -frompw himom -to other.example.com:993 -touser bar@other.example.com -topw hidad`
+`mvimap --once example.com:993 foo@example.com himom other.example.com:993 bar@other.example.com hidad`
 
 ### Keep copying IMAP Inbox to IMAP Inbox
-`mvimap -copy -from example.com:993 -fromuser foo@example.com -frompw himom -to other.example.com:993 -touser bar@other.example.com -topw hidad`
+`mvimap --copy example.com:993 foo@example.com himom other.example.com:993 bar@other.example.com hidad`
 
 ### Copy IMAP Inbox to IMAP Inbox Once
-`mvimap -copy -once -from example.com:993 -fromuser foo@example.com -frompw himom -to other.example.com:993 -touser bar@other.example.com -topw hidad`
+`mvimap --copy --once example.com:993 foo@example.com himom other.example.com:993 bar@other.example.com hidad`
+
+### Help
+```
+mvimap v0.1.0 - Move (or copy) messages from IMAP to IMAP
+Usage:  mvimap [Options] FROM_SERVER FROM_USER FROM_PW TO_SERVER TO_USER TO_PW
+  If FROM_SERVER or TO_SERVER does not use port 993, then append :PORT
+  Options:
+    --once:              Only do this once [default: keep running every minute]
+    --copy:              Copy messages [default: move messages]
+    --verbose:           Print verbose debug logs to stdout
+    --frombox FROM_BOX:  The source folder [default: INBOX]
+    --tobox TO_BOX:      The destination folder [default: INBOX]
+    -h|--help:           Only show this help text
+    -V|--version:        Only show the app version
+```
+
+## Install
+### Go install (if Golang is installed properly)
+`go install github.com/pepa65/mvimap@latest`
+
+# Go clone/install (if Golang is installed properly)
+`git clone https://github.com/pepa65/mvimap; cd mvimap; CGO_ENABLED=0 go install`
+
+# Smaller binaries
+```
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w"
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -ldflags="-s -w" -o mvimap_pi
+CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -ldflags="-s -w" -o mvimap_freebsd
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o mvimap_osx
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o mvimap.exe
+```
+
+# More extreme shrinking:
+`upx --best --lzma mvimap*`
+
+# Move them to the local binary directory (if in your PATH):
+`mv mvimap* ~/bin/`
+
+# Or move to a manually managed binaries location:
+`sudo mv mvimap* /usr/local/bin/`
 
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -13,39 +14,8 @@ import (
 	"github.com/emersion/go-imap/client"
 )
 
-func fetchForEach(c *client.Client, start int, end int, fetchItems []imap.FetchItem, f func(message *imap.Message) error) error {
-	var set imap.SeqSet
-	set.AddRange(uint32(start), uint32(end))
-	m := make(chan *imap.Message, end-start)
-	done := make(chan error, 1)
-	go func() {
-		done <- c.Fetch(&set, fetchItems, m)
-	}()
-	for msg := range m {
-		if err := f(msg); err != nil {
-			return err
-		}
-	}
-	return <-done
-}
-
-func remove(c *client.Client, q ...int) error {
-	if len(q) == 0 {
-		return nil
-	}
-	var set imap.SeqSet
-	for _, v := range q {
-		set.AddNum(uint32(v))
-	}
-	item := imap.FormatFlagsOp(imap.AddFlags, true)
-	flags := []interface{}{imap.DeletedFlag}
-	err := c.Store(&set, item, flags, nil)
-	if err != nil {
-		return err
-	}
-	err = c.Expunge(nil)
-	return err
-}
+const version = "v0.1.0"
+const name = "mvimap"
 
 type Config struct {
 	srcURL      string
@@ -62,26 +32,55 @@ type Config struct {
 }
 
 var logOut = log.New(os.Stdout, "", 0)
-var logErr = log.New(os.Stderr, "err: ", 0)
+var logErr = log.New(os.Stderr, "Error: ", 0)
 
 func main() {
 	var c Config
-	flag.StringVar(&c.srcURL, "from", "", "Source server")
-	flag.StringVar(&c.srcUsername, "fromuser", "", " Source username")
-	flag.StringVar(&c.srcPassword, "fromp", "", "Source password")
-	flag.StringVar(&c.srcFolder, "frombox", imap.InboxName, "Source folder")
-	flag.StringVar(&c.dstURL, "to", "", "Destination server")
-	flag.StringVar(&c.dstUsername, "touser", "", "Destination username")
-	flag.StringVar(&c.dstPassword, "topw", "", "Destination password")
+	flag.StringVar(&c.srcFolder, "frombox", imap.InboxName, "Source folder [default: INBOX]")
 	flag.StringVar(&c.dstFolder, "tobox", "", "Destination [default: same as Source folder]")
 	flag.BoolVar(&c.once, "once", false, "Only do this once")
 	flag.BoolVar(&c.copy, "copy", false, "Copy messages instead of moving")
 	flag.BoolVar(&c.verbose, "verbose", false, "Print verbose debug logs to stdout")
+	showVersion := flag.Bool("V", false, "Print version and exit")
+	flag.BoolVar(showVersion, "version", false, "Print version and exit")
+	flag.Usage = func() {
+		logOut.Printf("%s %s - Move (or copy) messages from IMAP to IMAP\n", name, version)
+		logOut.Printf("Usage:  %s [Options] FROM_SERVER FROM_USER FROM_PW TO_SERVER TO_USER TO_PW\n", name)
+		logOut.Println("  If FROM_SERVER or TO_SERVER does not use port 993, then append :PORT")
+		logOut.Println("  Options:")
+		logOut.Println("    --once:              Only do this once [default: keep running every minute]")
+		logOut.Println("    --copy:              Copy messages [default: move messages]")
+		logOut.Println("    --verbose:           Print verbose debug logs to stdout")
+		logOut.Println("    --frombox FROM_BOX:  The source folder [default: INBOX]")
+		logOut.Println("    --tobox TO_BOX:      The destination folder [default: INBOX]")
+		logOut.Println("    -h|--help:           Only show this help text")
+		logOut.Println("    -V|--version:        Only show the app version")
+	}
 	flag.Parse()
+
+	if *showVersion {
+		logOut.Println(name, version)
+		os.Exit(0)
+	}
+	if flag.NArg() != 6 {
+		logErr.Printf("Need 6 arguments: FROM_SERVER FROM_USER FROM_PW TO_SERVER TO_USER TO_PW")
+		os.Exit(1)
+	}
+	c.srcURL = flag.Arg(0)
+	c.srcUsername = flag.Arg(1)
+	c.srcPassword = flag.Arg(2)
+	c.dstURL = flag.Arg(3)
+	c.dstUsername = flag.Arg(4)
+	c.dstPassword = flag.Arg(5)
 	if c.dstFolder == "" {
 		c.dstFolder = c.srcFolder
 	}
-
+	if strings.IndexByte(c.srcURL, ':') >= 0 {
+		c.srcURL += ":993"
+	}
+	if strings.IndexByte(c.dstURL, ':') >= 0 {
+		c.dstURL += ":993"
+	}
 	for {
 		err := run(&c)
 		if err != nil {
@@ -94,34 +93,37 @@ func main() {
 }
 
 func run(c *Config) error {
-	dc, err := client.DialTLS(c.dstURL, &tls.Config{
+	to, err := client.DialTLS(c.dstURL, &tls.Config{
 		InsecureSkipVerify: true,
 	})
 	if err != nil {
 		return fmt.Errorf("dialing dst: %v", err)
 	}
-	defer dc.Logout()
-	if err := dc.Login(c.dstUsername, c.dstPassword); err != nil {
+
+	defer to.Logout()
+	if err := to.Login(c.dstUsername, c.dstPassword); err != nil {
 		return fmt.Errorf("login to dst: %v", err)
 	}
+
 	// select the mailbox to check if it exists; if it does not, create it.
-	if _, err := dc.Select(c.dstFolder, true); err != nil {
-		if err := dc.Create(c.dstFolder); err != nil {
+	if _, err := to.Select(c.dstFolder, true); err != nil {
+		if err := to.Create(c.dstFolder); err != nil {
 			return fmt.Errorf("creating dst mailbox: %v", err)
 		}
 	}
 
-	uc, err := client.DialTLS(c.srcURL, &tls.Config{
+	from, err := client.DialTLS(c.srcURL, &tls.Config{
 		InsecureSkipVerify: true,
 	})
 	if err != nil {
 		return fmt.Errorf("dialing src: %v", err)
 	}
-	defer uc.Logout()
-	if err := uc.Login(c.srcUsername, c.srcPassword); err != nil {
+	defer from.Logout()
+	if err := from.Login(c.srcUsername, c.srcPassword); err != nil {
 		return fmt.Errorf("login to src: %v", err)
 	}
-	mbox, err := uc.Select(c.srcFolder, c.copy)
+
+	mbox, err := from.Select(c.srcFolder, c.copy)
 	if err != nil {
 		return fmt.Errorf("selecting src mailbox: %v", err)
 	}
@@ -131,10 +133,9 @@ func run(c *Config) error {
 	}
 	updates := make(chan client.Update, 64)
 	updatesClose := make(chan struct{}, 1)
-	uc.Updates = updates
+	from.Updates = updates
 	defer close(updatesClose)
 	defer updateCond.Broadcast()
-
 	lastCount := 0
 	newCount := int(mbox.Messages)
 
@@ -169,26 +170,24 @@ func run(c *Config) error {
 		newCountLocal := newCount
 		lastCountLocal := lastCount
 		updateCond.L.Unlock()
-
 		if newCountLocal > lastCountLocal {
 			if c.verbose {
 				logOut.Printf("processing src messages %v to %v", lastCountLocal+1, newCountLocal)
 			}
-
 			bodySection := &imap.BodySectionName{
 				Peek: true,
 			}
 			items := []imap.FetchItem{imap.FetchFlags, imap.FetchInternalDate, imap.FetchRFC822Size, imap.FetchEnvelope, imap.FetchBody, bodySection.FetchItem()}
-
 			var removeIDs []int
-			err := fetchForEach(uc, lastCountLocal+1, newCountLocal, items, func(msg *imap.Message) error {
+			err := fetchForEach(from, lastCountLocal+1, newCountLocal, items, func(msg *imap.Message) error {
 				if c.verbose {
 					logOut.Printf("appending message %v to dst", int(msg.SeqNum))
 				}
-				if err := dc.Append(c.dstFolder, msg.Flags, msg.InternalDate, msg.GetBody(bodySection)); err != nil {
+				if err := to.Append(c.dstFolder, msg.Flags, msg.InternalDate, msg.GetBody(bodySection)); err != nil {
 					logErr.Printf("appending message to dst: %v", err)
 					return nil
 				}
+
 				if !c.copy {
 					removeIDs = append(removeIDs, int(msg.SeqNum))
 				}
@@ -197,12 +196,14 @@ func run(c *Config) error {
 			if err != nil {
 				return fmt.Errorf("fetching src messages: %v", err)
 			}
+
 			updateCond.L.Lock()
 			lastCount = newCountLocal
 			updateCond.L.Unlock()
-			if err := remove(uc, removeIDs...); err != nil {
+			if err := remove(from, removeIDs...); err != nil {
 				return fmt.Errorf("deleting src messages: %v", err)
 			}
+
 			if c.verbose {
 				logOut.Printf("processed src messages, src now has %v messages", newCountLocal)
 			}
@@ -210,18 +211,22 @@ func run(c *Config) error {
 		if c.once {
 			return nil
 		}
+
 		errCh := make(chan error, 1)
 		updateCh := make(chan struct{}, 1)
 		idleCh := make(chan struct{})
+
 		go func() {
-			errCh <- uc.Idle(idleCh, nil)
+			errCh <- from.Idle(idleCh, nil)
 		}()
+
 		go func() {
 			updateCond.L.Lock()
 			updateCond.Wait()
 			updateCond.L.Unlock()
 			updateCh <- struct{}{}
 		}()
+
 		select {
 		case err := <-errCh:
 			if err != nil {
@@ -231,4 +236,38 @@ func run(c *Config) error {
 		}
 		close(idleCh)
 	}
+}
+
+func fetchForEach(c *client.Client, start int, end int, fetchItems []imap.FetchItem, f func(message *imap.Message) error) error {
+	var set imap.SeqSet
+	set.AddRange(uint32(start), uint32(end))
+	m := make(chan *imap.Message, end-start)
+	done := make(chan error, 1)
+	go func() {
+		done <- c.Fetch(&set, fetchItems, m)
+	}()
+	for msg := range m {
+		if err := f(msg); err != nil {
+			return err
+		}
+	}
+	return <-done
+}
+
+func remove(c *client.Client, q ...int) error {
+	if len(q) == 0 {
+		return nil
+	}
+	var set imap.SeqSet
+	for _, v := range q {
+		set.AddNum(uint32(v))
+	}
+	item := imap.FormatFlagsOp(imap.AddFlags, true)
+	flags := []interface{}{imap.DeletedFlag}
+	err := c.Store(&set, item, flags, nil)
+	if err != nil {
+		return err
+	}
+	err = c.Expunge(nil)
+	return err
 }
